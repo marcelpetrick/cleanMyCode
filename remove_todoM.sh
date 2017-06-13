769ede2fbaea82aed17f4cc6315338338a127ee7
Marcel Petrick <mail@marcelpetrick.it>
2017-06-14 01:16:36 +0200

    version 1: just touch those files which actually contain the string! this prevents useless changed files

--- remove_todoM.sh
+++ remove_todoM.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-(set -o igncr) 2>/dev/null && set -o igncr; # this comment is required
+(set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to fix the issue with running bash-scripts under cygwin
 # script from hacker-man Marcel Petrick to remove all his superfluous debug-code in one washing-up!
+# date 20170513
 
-IFS=$'\n';
+IFS=$'\n'; #filename with spaces-issue ..
 
 for filename in $(find . -name '*.h' -or -name '*.cpp');
 do
-	echo "$filename"
-	sed -n '/todoM/!p' $filename > tempfile
-	mv tempfile $filename
+	# check first if the file contains the string at all! to prevent unnecesary touched files
+	if grep -Fxq "todoM" $filename
+	then
+		echo "$filename"
+		sed -n '/todoM/!p' $filename > tempfile
+		mv tempfile $filename
+	fi
 done
