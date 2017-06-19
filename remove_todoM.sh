0a5f11acb6de61fdc91d802eb21dba4ca07bbdf8
Marcel Petrick <mail@marcelpetrick.it>
2017-06-19 22:13:23 +0200

    version 3: added simple example of how to use GNU parallel

--- remove_todoM.sh
+++ remove_todoM.sh
@@ -2,13 +2,23 @@
 (set -o igncr) 2>/dev/null && set -o igncr; # this comment is required to fix the issue with running bash-scripts under cygwin
 
 # script from hacker-man Marcel Petrick to remove all his superfluous debug-code in one washing-up!
-# date: 20170513
-# version: v2
+# date: 20170519
+# version: v3
 
 # todo: make the searchstring case-independent
 # todo: make it use GNU parallel
 
 IFS=$'\n'; #deal with the "filename with spaces"-issue ..
+
+doit() {
+	echo Doing it for $1
+	sleep 2
+	echo Done with $1
+ }
+ export -f doit
+ parallel  -j8 --bar doit ::: 1 2 3 5 6 
+ 
+ exit 1
 
 # go over all subdirectories
 for filename in $(find . -name '*.h' -or -name '*.cpp'); do
@@ -19,4 +29,3 @@ for filename in $(find . -name '*.h' -or -name '*.cpp'); do
 		mv tempfile $filename
 	fi
 done
-
